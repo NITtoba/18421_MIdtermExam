a9cb6939acd018bf0d683c71575198d4d2696d55
18421NakayamaKota <72592606+18421NakayamaKota@users.noreply.github.com>
2021-11-17 14:40:20 +0900

    変更しました

--- tcp_c.py
+++ tcp_c.py
@@ -2,6 +2,6 @@ import socket
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as SOCK:
     SOCK.connect(('127.0.0.1', 50007))
-    SOCK.sendall(b'client to server')
+    SOCK.sendall(b'hello server')
     DatA = SOCK.recv(1024)
     print(repr(DatA))
