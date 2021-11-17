769f1dce69bb24d706a1e2519c11dd063cfce497
18421NakayamaKota <18421@toba-cmt.ac.jp>
2021-11-17 14:57:59 +0900

    PEP8に則り、4つの変更点をcommit

--- tcp_c.py
+++ tcp_c.py
@@ -1,7 +1,7 @@
 import socket
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as SOCK:
-    SOCK.connect(('127.0.0.1', 50007))
-    SOCK.sendall(b'hello server')
-    DatA = SOCK.recv(1024)
-    print(repr(DatA))
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    sock.connect(('127.0.0.1', 50007))
+    sock.sendall(b"hello server")
+    data = sock.recv(1024)
+    print(repr(data))
