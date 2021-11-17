17d8365946bd33a213214034823191de7076f8d6
18421NakayamaKota <72592606+18421NakayamaKota@users.noreply.github.com>
2021-11-17 14:42:18 +0900

    変更しました

--- udp_c.py
+++ udp_c.py
@@ -1,5 +1,5 @@
 import socket
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as BbBbB:
-    BbBbB.sendto(b'from c', ('127.0.0.1', 50007))
+    BbBbB.sendto(b'from client message', ('127.0.0.1', 50007))
     
